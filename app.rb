28def1af75135c42dadd9fff3b64cc3345dc2f2a
Nathan Smith <nsmith@missiondata.com>
2023-06-05 08:44:20 -0700

    Add cake list implementations using embedded ruby and json/jquery

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'sinatra/json'
 
+# View cakes using embedded Ruby
 get '/' do
-  "Hello, world"
+  cakeList = File.open('cake.list')
+  @cakes = cakeList.readlines.map(&:chomp)
+  erb :index
+end
+
+# View cakes by hitting cakes.json and updating DOM with JQuery
+get '/jsoncakes' do
+  erb :jsoncakes
+end
+
+# Return cakes list as a JSONified array
+get '/cakes.json' do
+  cakeList = File.open('cake.list')
+  @cakes = cakeList.readlines.map(&:chomp)
+  @cakes.to_json
 end
